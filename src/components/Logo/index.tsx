ccc77b95cabe5e4fbfbc4a968a551c454a331903
alyssondepaula <alysson.depaula@outlook.com>
2020-12-14 00:45:55 -0300

    screenLandingPage

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, ImageLogo, TextLogo } from './styles';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Logo: React.FC = () => {
   return (
     <Container>
-      <Text>Logo</Text>
+      <ImageLogo>
+<FontAwesome5 name="dog" size={156} color="white" />
+      </ImageLogo>
+
+      <TextLogo>Adote s2</TextLogo>
     </Container>
   );
 };
--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import { Dimensions } from 'react-native';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export const Container = styled.View`
+  display: flex;
+  flex-direction: column;
+  width: ${ windowWidth * 0.95}px;
+  height: ${ windowHeight * 0.35}px;
+  padding: 4px;
+  align-items: center;
+`;
+
+export const ImageLogo = styled.View`
+  align-items: center;
+`;
+
+export const TextLogo = styled.Text`
+  color:  white;
+  font-weight: bold;
+  font-size: 24px;
 `;
